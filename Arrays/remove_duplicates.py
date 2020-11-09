7c9f70e34890151fda51c6409ab2254aad99c73b
Ruth Poliakon <r.poliakon@gmail.com>
2020-11-09 13:36:29 -0700

    remove_duplicates solution

---
+++ Arrays/remove_duplicates.py
@@ -0,0 +1,52 @@
+"""
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+"""
+# important notes:
+## array is SORTED
+# have to remove duplicates IN PLACE
+
+
+def removeDuplicates(nums):
+    index = 1
+    while index < len(nums):
+        if nums[index] == nums[index - 1]:
+            nums.pop(index - 1)
+            index -= 1
+        index += 1
+    return nums
+
+
+# first pass (doesn't work with triplets+)
+"""
+def removeDuplicates(nums):
+    # iterate through the array
+    # if any number matches the number before it, remove that number from the array
+    for index, num in enumerate(nums):
+        if nums[index] == nums[index - 1]:
+            nums.remove(num)
+
+    return nums
+"""
+
+
+print(removeDuplicates([1, 2, 2, 2, 3, 4, 5, 5, 6]))
